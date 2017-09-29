d4beca27059aa7cb45de57760f9e4a7ad95288e6
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-09-28 17:46:53 -0700

    Branch label starts with owner not repo

--- index.js
+++ index.js
@@ -27,17 +27,22 @@ async function hasPushAccess (context, params) {
 
 module.exports = (robot) => {
   robot.on('pull_request.opened', async context => {
-    const {repo} = context.repo()
+    const {owner} = context.repo()
     const branchLabel = context.payload.pull_request.head.label
 
-    // If the branch label starts with the repo name, then it is a PR from a branch in the local
+    // If the branch label starts with the owner name, then it is a PR from a branch in the local
     // repo
-    if (branchLabel.startsWith(repo)) {
-      const canPush = await hasPushAccess(context.repo({username: context.payload.pull_request.user.login}))
+    if (branchLabel.startsWith(owner)) {
+      robot.log.debug(`PR created from branch in the local repo`)
+
+      const username = context.payload.pull_request.user.login
+      const canPush = await hasPushAccess(context, context.repo({username}))
 
       // If the user creating the PR from a local branch doesn't have push access, then they
       // can't push to their own PR and it isn't going to be useful
       if (!canPush) {
+        robot.log.debug(`PR created from repo branch and user cannot push - closing PR`)
+
         await comment(context, context.issue({body: commentBody}))
 
         return close(context, context.issue())
