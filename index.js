4d5d10d1f965c3788ca4d30b00fc4517c33a5fe8
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-07-24 10:39:27 -0700

    robot => app

--- index.js
+++ index.js
@@ -47,24 +47,24 @@ async function hasPushAccess (context, params) {
   return level === 'admin' || level === 'write'
 }
 
-module.exports = (robot) => {
-  robot.on('pull_request.opened', async context => {
+module.exports = (app) => {
+  app.on('pull_request.opened', async context => {
     const config = await getConfig(context, 'mistaken-pull-closer.yml', defaultConfig) || defaultConfig
     const {owner} = context.repo()
     const branchLabel = context.payload.pull_request.head.label
 
-    robot.log.debug(`Inspecting: ${context.payload.pull_request.html_url}`)
+    app.log.debug(`Inspecting: ${context.payload.pull_request.html_url}`)
 
     // If the branch label starts with the owner name, then it is a PR from a branch in the local
     // repo
     if (branchLabel.startsWith(owner)) {
-      robot.log.debug(`PR created from branch in the local repo`)
+      app.log.debug(`PR created from branch in the local repo`)
       const user = context.payload.pull_request.user
 
       // If the user is a bot then it was invited to open pull requests and isn't the
       // kind of mistake this bot was intended to detect
       if (user.type !== 'Bot') {
-        robot.log.debug(`User creating the PR is not a bot`)
+        app.log.debug(`User creating the PR is not a bot`)
 
         const username = user.login
         const canPush = await hasPushAccess(context, context.repo({username}))
@@ -72,7 +72,7 @@ module.exports = (robot) => {
         // If the user creating the PR from a local branch is not a bot and doesn't have push
         // access, then they can't push to their own PR and it isn't going to be useful
         if (!canPush) {
-          robot.log.debug(`PR created from repo branch and user cannot push - closing PR`)
+          app.log.debug(`PR created from repo branch and user cannot push - closing PR`)
 
           await comment(context, context.issue({body: config.commentBody}))
           if (config.addLabel) {
